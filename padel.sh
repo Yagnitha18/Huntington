fa1675575afaa5bc9297fd5b6fbb5f1fb4709ff5
yagnitha <98573889+yagnitha@users.noreply.github.com>
2022-02-16 18:17:53 +0530

    Update padel.sh

--- padel.sh
+++ padel.sh
@@ -1,1 +1,2 @@
 java -Xms1G -Xmx1G -Djava.awt.headless=true -jar ./PaDEL-Descriptor/PaDEL-Descriptor.jar -removesalt -standardizenitro -fingerprints -descriptortypes ./PaDEL-Descriptor/PubchemFingerprinter.xml 
+-dir ./ -file descriptors_output.csv
